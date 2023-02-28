2036f2d4d2a97bcc73e107fc3be8987fbae801ba
NowakArtur97 <nowart97@gmail.com>
2023-02-28 12:17:44 +0100

    Setup Cloud Formation config with Lambda, S3 Bucket and Log Group

--- reports_sender.py
+++ reports_sender.py
@@ -3,6 +3,7 @@ import json
 import urllib.parse
 import boto3
 import email
+import cfnresponse
 from botocore.exceptions import ClientError
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -10,16 +11,34 @@ from email.mime.application import MIMEApplication
 
 print('Loading function')
 
+SENDER = os.environ['SENDER']
+EMAIL_SUBJECT = os.environ['EMAIL_SUBJECT']
+RECIPIENTS = os.environ['RECIPIENTS'].split(",")
+REGION = os.environ['REGION']
+BUCKET = os.environ['BUCKET_NAME']
+
 s3 = boto3.client('s3')
 
-def send_email(sender, recipient, aws_region, subject, file_name):
-    BODY_TEXT = "Hello,\r\nPlease find the attached file."
+def clear_bucket():
+    s3.Bucket(BUCKET).objects.all().delete()
+    print("Successfully cleared bucket: " + BUCKET)
+
+def download_report(event):
+    KEY = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    fileName = os.path.basename(KEY)
+    tmpFileName = '/tmp/' +fileName
+    s3.download_file(BUCKET, KEY, tmpFileName)
+    print("Successfully downloaded the report from bucket: " + BUCKET)
+    return tmpFileName
+
+def send_report(sender, recipient, aws_region, subject, file_name):
+    BODY_TEXT = "Hello,\r\nPlease find the attached report."
     BODY_HTML = """\
     <html>
     <head></head>
     <body>
-    <h1>Hello!</h1>
-    <p>Please find the attached file.</p>
+    <h1>Hello</h1>
+    <p>Please find the attached report.</p>
     </body>
     </html>
     """
@@ -37,40 +56,44 @@ def send_email(sender, recipient, aws_region, subject, file_name):
     att = MIMEApplication(open(file_name, 'rb').read())
     att.add_header('Content-Disposition','attachment', filename="report.csv")
     if os.path.exists(file_name):
-        print("File exists")
+        print("File: " + file_name + " exists")
+        msg.attach(msg_body)
+        msg.attach(att)
+        client.send_raw_email(
+                Source=msg['From'],
+                Destinations=[
+                    msg['To']
+                ],
+                RawMessage={
+                    'Data':msg.as_string(),
+                }
+            )
+        print('Reports sent successfully')
     else:
-        print("File does not exists")
-    msg.attach(msg_body)
-    msg.attach(att)
-    try:
-        response = client.send_raw_email(
-            Source=msg['From'],
-            Destinations=[
-                msg['To']
-            ],
-            RawMessage={
-                'Data':msg.as_string(),
-            }
-        )
-    except ClientError as e:
-        print(e.response['Error']['Message'])
-    else:
-        print("Email sent! Message ID:", response['MessageId'])
+        message = "File: " + file_name + " does not exist"
+        print(message)
+        raise Exception(message)
 
 def lambda_handler(event, context):
-    BUCKET = event['Records'][0]['s3']['bucket']['name']
-    KEY = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    responseData = {}
+    attachment = {}
+    if 'RequestType' in event:
+        try:
+            if event['RequestType'] == 'Delete':
+                clear_bucket()
+            cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData)
+        except Exception as e:
+            print('Exception when setting up the infrastructure')
+            print(e)
+            cfnresponse.send(event, context, cfnresponse.FAILED, responseData)
     try:
-        response = s3.get_object(Bucket=BUCKET, Key=KEY)
-        print("CONTENT TYPE: " + response['ContentType'])
-        FILE_NAME = os.path.basename(KEY)
-        TMP_FILE_NAME = '/tmp/' +FILE_NAME
-        s3.download_file(BUCKET, KEY, TMP_FILE_NAME)
-        ATTACHMENT = TMP_FILE_NAME
-        print(ATTACHMENT)
-        send_email('email@gmail.com', 'email@gmail.com', 'eu-central-1', 'AWS LAMBDA TEST', ATTACHMENT)
-        return response['ContentType']
+        attachment = download_report(event)
     except Exception as e:
+        print('Exception when fetching report from bucket')
         print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(KEY, BUCKET))
-        raise e
+    try:
+        for recipient in RECIPIENTS:
+            send_report(SENDER, recipient, REGION, EMAIL_SUBJECT, attachment)
+    except Exception as e:
+        print('Exception when sending reports')
+        print(e)
