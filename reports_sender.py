edb9596cab26ed0de9d20e5ab0d98f00665c462b
NowakArtur97 <nowart97@gmail.com>
2023-03-02 09:54:19 +0100

    Create a custom resource to clean the S3 bucket when removing the CloudFormation stack

--- reports_sender.py
+++ reports_sender.py
@@ -2,7 +2,6 @@ import os.path
 import urllib.parse
 import boto3
 import email
-import cfnresponse
 from botocore.exceptions import ClientError
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -50,7 +49,6 @@ def send_report(sender, recipient, aws_region, subject, file_name):
     msg_body.attach(htmlpart)
     att = MIMEApplication(open(file_name, 'rb').read())
     att.add_header('Content-Disposition','attachment', filename="report.csv")
-    print("File: " + file_name + " exists")
     msg.attach(msg_body)
     msg.attach(att)
     client.send_raw_email(
@@ -62,22 +60,18 @@ def send_report(sender, recipient, aws_region, subject, file_name):
                 'Data':msg.as_string(),
             }
         )
-    print('Report sent successfully')
+    print('Report: ' + file_name + ' sent successfully to: ' + recipient)
 
 def lambda_handler(event, context):
-    responseData = {}
     attachment = {}
-    if 'RequestType' in event:
-        cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData)
-    else:
-        try:
-            attachment = download_report(event)
-        except Exception as e:
-            print('Exception when fetching report from bucket')
-            print(e)
-        try:
-            for recipient in RECIPIENTS:
-                send_report(SENDER, recipient, REGION, EMAIL_SUBJECT, attachment)
-        except Exception as e:
-            print('Exception when sending reports')
-            print(e)
+    try:
+        attachment = download_report(event)
+    except Exception as e:
+        print('Exception when fetching report from bucket')
+        print(e)
+    try:
+        for recipient in RECIPIENTS:
+            send_report(SENDER, recipient, REGION, EMAIL_SUBJECT, attachment)
+    except Exception as e:
+        print('Exception when sending reports')
+        print(e)
